c0d041e002ce044cb6b6360ed2f78be2ffd6deed
papchenko <rocketmannot@gmail.com>
2019-04-02 10:52:48 +0300

    LWDEV-10494 add tests

--- src/test/kotlin/com/lykke/client/accounts/ClientAccountsCacheTest.kt
+++ src/test/kotlin/com/lykke/client/accounts/ClientAccountsCacheTest.kt
@@ -25,14 +25,20 @@ class ClientAccountsCacheTest {
         assertEquals("Client1", clientAccountsCache.getClientByWalletId("testWallet1"))
         assertEquals("Client2", clientAccountsCache.getClientByWalletId("testWallet2"))
         assertEquals("Client3", clientAccountsCache.getClientByWalletId("testWallet3"))
+
+        assertEquals(setOf("testWallet3"), clientAccountsCache.getWalletsByClientId("Client3"))
     }
 
     @Test
     fun testDeleteEntryFromCache() {
         val clientAccountsCache =
             ClientAccountsCacheImpl(ConcurrentHashMap(mapOf("testWallet1" to "Client1", "testWallet2" to "Client2")))
+
         clientAccountsCache.delete("testWallet1")
         assertNull(clientAccountsCache.getClientByWalletId("testWallet1"))
         assertEquals("Client2", clientAccountsCache.getClientByWalletId("testWallet2"))
+
+        assertEquals(emptySet<String>(), clientAccountsCache.getWalletsByClientId("Client1"))
+        assertEquals(setOf("testWallet2"), clientAccountsCache.getWalletsByClientId("Client2"))
     }
 }
